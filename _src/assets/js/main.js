649664accde021efacfb945f5033c0907f89872c
desigc <desi.gc93@gmail.com>
2018-12-09 19:51:53 +0100

    Add js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-console.log('>> Ready :)');
+const userInputEl = document.querySelector('.nickUser');
+const btnEl = document.querySelector('.btn');
+const listEl = document.querySelector('.list');
+
+
+
+function printNameUser (stringText) {
+    let listContent = "";
+    for (let i=0; i < stringText.length; i++){
+        let letterContent = `<li class="list__item"> ${stringText[i]} </li>`;
+        console.log(stringText[i]);
+        listContent += letterContent;
+
+    }
+    listEl.innerHTML = listContent;
+}
+
+function searchUser (event){
+    event.preventDefault();
+    console.log('nombre de la usuaria', userInputEl.value);
+    fetch(`https://api.github.com/users/${userInputEl.value}`)
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data);
+        let nameUser = data.name;
+        console.log('nombre de la usuaria',nameUser);
+        let splitNameUser = nameUser.split(" ");
+        console.log('la cadena del nombre',splitNameUser);
+        printNameUser (splitNameUser[0]);
+
+    });
+
+}
+
+
+
+btnEl.addEventListener('click', searchUser);
+
